a5b0415f327cdfb7e408b0abe0e4da941018ba45
Jairo Alan Castelan Fernandez <jairoalanc@outlook.com>
2024-05-31 20:45:31 -0600

    Breve Explicacion del codigo

--- app/src/main/java/com/example/calificacines/BaseDeDatos.java
+++ app/src/main/java/com/example/calificacines/BaseDeDatos.java
@@ -1,15 +1,18 @@
 package com.example.calificacines;
 
+// Librerias que se utilizaran al crear una base de datos
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
 import androidx.annotation.Nullable;
-
+// Clase BaseDeDatos que hereda de SQLiteOpenHelper
 public class BaseDeDatos extends SQLiteOpenHelper
 {
+    // Clase abstracta que contendra todas los dato de la tabla asi como su nombre
     public static abstract class Datostabla implements BaseColumns {
+        // Atributos la base de datos
         public static final String Nombre_Table = "Datos";
         public static final String Columna_Matr = "Matricula";
         public static final String Columna_Nombre = "Nombre";
@@ -19,6 +22,7 @@ public class BaseDeDatos extends SQLiteOpenHelper
         public static final String Columna_Parcial2 = "Parcial2";
         public static final String Columna_Parcial3 = "Parcial3";
 
+        // Crea una tabla que configura los atrubutos de cada columna
         private static final String Crear_Table = "create table " + Nombre_Table + " (" +
                 Columna_Matr + " integer primary key, " +
                 Columna_Nombre + " text not null, " +
@@ -28,10 +32,13 @@ public class BaseDeDatos extends SQLiteOpenHelper
                 Columna_Parcial2 + " integer, " +
                 Columna_Parcial3 + " integer" + ");";
 
+        // instruccion que borra la tabla
         private static final String sql_Delete = "drop table if exists " + Nombre_Table;
     }
 
+    // Version de la tabla, en mi caso es 2, por que la 1 tenia errores
     public static final int DataBase_Version=2;
+    // Nombre de la base de datos, que se guardara en el telefono
     public static final String DataBase_Name="directorio.db";
 
 
--- app/src/main/java/com/example/calificacines/MainActivity.java
+++ app/src/main/java/com/example/calificacines/MainActivity.java
@@ -16,8 +16,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    // Todos los Edittext que se ocuparan
     EditText etMatricula, etNombre, etMateria, etCarrera, etParcial1, etParcial2, etParcial3, etPromedio;
+    // Asi como los botones
     Button btnBuscar, btnInsertar, btnCalificaciones;
+
+    // Objeto de la clase Base de Datos
     BaseDeDatos ayudaDB;
 
     @Override
@@ -39,11 +43,13 @@ public class MainActivity extends AppCompatActivity {
         btnInsertar = findViewById(R.id.btnInsertar);
         btnCalificaciones = findViewById(R.id.btnCalificaciones);
 
+        // Referenciamos la SQlHelper
         ayudaDB = new BaseDeDatos(getApplicationContext());
 
         btnBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Metodo para buscarel alumno en la base de datos
                 buscarAlumno();
             }
         });
@@ -51,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         btnInsertar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Metodo para insertar el alumno en la base de datos
                 insertarAlumno();
             }
         });
@@ -58,19 +65,26 @@ public class MainActivity extends AppCompatActivity {
         btnCalificaciones.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Metodo para registrar la calificacion del alumno en la base de datos
                 registrarCalificacion();
             }
         });
     }
 
+    // Metodo buscarAlumno
     private void buscarAlumno() {
+        // La busqueda se hace con la matricula del alumno
         String matricula = etMatricula.getText().toString();
+        // Si la matricula esta vacia
         if (matricula.isEmpty()) {
+            // Imprime un mensaje
             Toast.makeText(this, "Ingrese una matrícula", Toast.LENGTH_LONG).show();
             return;
         }
-
+        // Si no, se crea una variable db que lea la base de datos
         SQLiteDatabase db = ayudaDB.getReadableDatabase();
+        // Objeto cursor que ejecuta el query para la base de datos. En nuestro caso,
+        // Es el nombre de la tabla donde se va a realizar la consulta.
         Cursor cursor = db.query(BaseDeDatos.Datostabla.Nombre_Table,
                 null,
                 BaseDeDatos.Datostabla.Columna_Matr + " = ?",
@@ -83,28 +97,33 @@ public class MainActivity extends AppCompatActivity {
             etNombre.setText(cursor.getString(cursor.getColumnIndexOrThrow(BaseDeDatos.Datostabla.Columna_Nombre)));
             etMateria.setText(cursor.getString(cursor.getColumnIndexOrThrow(BaseDeDatos.Datostabla.Columna_Materia)));
             etCarrera.setText(cursor.getString(cursor.getColumnIndexOrThrow(BaseDeDatos.Datostabla.Columna_Carrera)));
+            // Este código verifica si el valor de la columna es NULL. Si es NULL, se establece un texto vacío (""). Si no es NULL, se obtiene el valor de la columna y se establece en el EditText correspondiente.
             etParcial1.setText(cursor.isNull(cursor.getColumnIndexOrThrow(BaseDeDatos.Datostabla.Columna_Parcial1)) ? "" : cursor.getString(cursor.getColumnIndexOrThrow(BaseDeDatos.Datostabla.Columna_Parcial1)));
             etParcial2.setText(cursor.isNull(cursor.getColumnIndexOrThrow(BaseDeDatos.Datostabla.Columna_Parcial2)) ? "" : cursor.getString(cursor.getColumnIndexOrThrow(BaseDeDatos.Datostabla.Columna_Parcial2)));
             etParcial3.setText(cursor.isNull(cursor.getColumnIndexOrThrow(BaseDeDatos.Datostabla.Columna_Parcial3)) ? "" : cursor.getString(cursor.getColumnIndexOrThrow(BaseDeDatos.Datostabla.Columna_Parcial3)));
-
+            // Y calcula el promedio con el metodo..
             calcularPromedio();
         } else {
+            // Mesaje si no se encuentra el alumno
             Toast.makeText(this, "Alumno no encontrado", Toast.LENGTH_LONG).show();
         }
+        // Cierra el cursor para liberar los recursos asociados.
         cursor.close();
     }
 
+    // Metodo para insertar el alumno
     private void insertarAlumno() {
         String matricula = etMatricula.getText().toString();
         String nombre = etNombre.getText().toString();
         String materia = etMateria.getText().toString();
         String carrera = etCarrera.getText().toString();
 
+        // Si la matricula y los demas edittext estan vacios, imprime un mensaje
         if (matricula.isEmpty() || nombre.isEmpty() || materia.isEmpty() || carrera.isEmpty()) {
             Toast.makeText(this, "Complete todos los campos", Toast.LENGTH_LONG).show();
             return;
         }
-
+        // Si no...
         SQLiteDatabase db = ayudaDB.getWritableDatabase();
         ContentValues valores = new ContentValues();
         valores.put(BaseDeDatos.Datostabla.Columna_Matr, matricula);
@@ -112,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
         valores.put(BaseDeDatos.Datostabla.Columna_Materia, materia);
         valores.put(BaseDeDatos.Datostabla.Columna_Carrera, carrera);
 
+        // Variable que guarda un "identificador", si es -1...
         long newRowId = db.insert(BaseDeDatos.Datostabla.Nombre_Table, null, valores);
         if (newRowId == -1) {
             Toast.makeText(this, "Error al guardar los datos", Toast.LENGTH_LONG).show();
@@ -120,14 +140,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    // Metodo para registrar la claificacion
     private void registrarCalificacion() {
+
         String matricula = etMatricula.getText().toString();
+        // Si la matricula esta vacia (Es necesario con la matricula)
         if (matricula.isEmpty()) {
             Toast.makeText(this, "Ingrese una matrícula", Toast.LENGTH_LONG).show();
             return;
         }
-
+        // Si no...
         SQLiteDatabase db = ayudaDB.getWritableDatabase();
         Cursor cursor = db.query(BaseDeDatos.Datostabla.Nombre_Table,
                 null,
@@ -139,16 +161,20 @@ public class MainActivity extends AppCompatActivity {
 
         if (cursor.moveToFirst()) {
             ContentValues valores = new ContentValues();
+            // Los edittext se guardan en las variables
             String parcial1 = etParcial1.getText().toString();
             String parcial2 = etParcial2.getText().toString();
             String parcial3 = etParcial3.getText().toString();
 
+            // parcial3 es diferente de vacio
             if (!parcial3.isEmpty()) {
+                // y si es nulo, la columna del parcial 2 ("Busca en la base de datos")
                 if (cursor.isNull(cursor.getColumnIndexOrThrow(BaseDeDatos.Datostabla.Columna_Parcial2))) {
                     Toast.makeText(this, "Debe registrar el segundo parcial antes de registrar el tercero", Toast.LENGTH_LONG).show();
                     cursor.close();
                     return;
                 }
+                // Si no...
                 valores.put(BaseDeDatos.Datostabla.Columna_Parcial3, Integer.parseInt(parcial3));
             } else if (!parcial2.isEmpty()) {
                 if (cursor.isNull(cursor.getColumnIndexOrThrow(BaseDeDatos.Datostabla.Columna_Parcial1))) {
@@ -184,6 +210,8 @@ public class MainActivity extends AppCompatActivity {
             String parcial2 = etParcial2.getText().toString();
             String parcial3 = etParcial3.getText().toString();
 
+            // Esta línea usa el operador ternario (? :) para verificar si la cadena parcial1 está
+            // vacía y, en función de eso, asigna un valor a p1, al comenzar es 0.
             int p1 = parcial1.isEmpty() ? 0 : Integer.parseInt(parcial1);
             int p2 = parcial2.isEmpty() ? 0 : Integer.parseInt(parcial2);
             int p3 = parcial3.isEmpty() ? 0 : Integer.parseInt(parcial3);
